f554f353cfafe884286534eb06a2a8e36ee35cd9
Sohee <sohee@example.com>
2026-04-19 22:20:30 +0900

    fix: change wildcard route for express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ setupSocketHandlers(io);
 
 // Serve static React build files in production
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
